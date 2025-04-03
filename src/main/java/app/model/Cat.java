95b5733729f85dbb054e38e6d604b44777a5a874
tricky <alavutdin055@gmail.com>
2025-04-03 23:33:46 +0300

    1

--- src/main/java/app/model/Cat.java
+++ src/main/java/app/model/Cat.java
@@ -1,8 +1,10 @@
 package app.model;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-
+// Создайте бин Dog, унаследуйте его от Animal. Создайте на основе этого класса компонент.
 @Component
+
 public class Cat extends Animal {
     @Override
     public String toString() {
